21b399ffb6e33f9c44edcce0ee751215c8e1c012
geoffreyjorissenpxl <geoffreyjorissen@msn.com>
2021-12-19 15:36:58 +0100

    eerste versie fruitShop

---
+++ Fruitshop/Fruitshop/Fruit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fruitshop
+{
+    public abstract class Fruit
+    {
+        public string Description { get; set; }
+        public double Weight { get; set; }
+
+    }
+}
